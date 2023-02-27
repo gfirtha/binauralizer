1b0cadeba69780b88fbd0a5b06f2173e143e6f3e
gfirtha <firtha@hit.bme.hu>
2023-02-27 15:41:08 +0100

    rework

---
+++ Files/Functions/get_default_renderer_setup.m
@@ -0,0 +1,18 @@
+function setup = get_default_renderer_setup(renderer_type)
+    switch renderer_type
+        case 'Direct_playback'
+            setup = [];
+        case 'VBAP'
+            setup = [];
+        case 'DBAP'
+            setup = [];
+        case 'WFS'
+            setup = struct('Antialiasing','off');
+        case 'CTC'
+            hrtf_sofa = SOFAload('BuK_ED_corr.sofa');
+            setup = struct( 'Plant_model','HRTF','VS_model','HRTF', 'HRTF_database',hrtf_sofa,'N_filt',4096);
+
+    end
+
+end
+
