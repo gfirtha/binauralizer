c68d65947749f9b4247411cdd38209b9a20a5a78
gfirtha <firtha@hit.bme.hu>
2023-03-22 15:01:37 +0100

    ..

--- Files/Functions/get_default_renderer_setup.m
+++ Files/Functions/get_default_renderer_setup.m
@@ -11,7 +11,7 @@ function setup = get_default_renderer_setup(renderer_type)
         case 'LWFS'
             setup = struct('Antialiasing','on');
         case 'NFC_HOA'
-            setup = struct('M',30);
+            setup = struct('M',20);
         case 'CTC'
             hrtf_sofa = SOFAload('BuK_ED_corr.sofa');
             setup = struct( 'Plant_model','HRTF','VS_model','HRTF', 'HRTF_database',hrtf_sofa,'N_filt',4096);
